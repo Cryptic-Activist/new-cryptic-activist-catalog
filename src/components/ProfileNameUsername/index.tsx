7eb86f8b289ebf7f49e1608ad2123bcd275efe71
Davi Silva <davi.cunha.silva.dev@gmail.com>
2023-07-06 15:04:15 +0200

    feature: improves spacing and shadow

--- src/components/ProfileNameUsername/index.tsx
+++ src/components/ProfileNameUsername/index.tsx
@@ -1,5 +1,7 @@
+import { Plus } from 'lucide-react';
 import { FC } from 'react';
 
+import { Button } from '@/components';
 import styles from './index.module.scss';
 import { ProfileNameUsername } from './types';
 
@@ -7,8 +9,14 @@ const ProfileNameUsername: FC<ProfileNameUsername> = ({ names, username }) => {
   const fullname = [names?.firstName ?? '', names?.lastName ?? ''].join(' ');
   return (
     <div className={styles.container}>
-      <h1 className={styles.name}>{fullname}</h1>
-      <h2 className={styles.username}>{username}</h2>
+      <div className={styles.column}>
+        <h1 className={styles.name}>{fullname}</h1>
+        <h2 className={styles.username}>{username}</h2>
+      </div>
+      <Button href="/offer/create" theme="ghost">
+        <p>Create Offer</p>
+        <Plus />
+      </Button>
     </div>
   );
 };
